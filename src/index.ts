1b3cc33854e95ae27b913fef7f6315d45e030fe5
NickUA <246246s@gmail.com>
2023-07-01 14:01:58 +0300

    feat: add internal server error handler

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,22 @@ server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-function requestHandler(req: IncomingMessage, res: ServerResponse) {
+async function requestHandler(req: IncomingMessage, res: ServerResponse) {
   res.setHeader("Content-Type", "application/json");
   console.debug(req.method, req.url)
-
-  if (req.url) {
-    if (req.url.slice(-1) === '/') { req.url = req.url.slice(0, -1) }
-    if (req.url.startsWith('/api/users')) {
-      usersRouter(req, res);
-      return
+  try {
+    if (req.url) {
+      if (req.url.slice(-1) === '/') { req.url = req.url.slice(0, -1) }
+      if (req.url.startsWith('/api/users')) {
+        await usersRouter(req, res);
+        return
+      }
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: `NOT FOUND ${req.url}` }));
     }
+  } catch (err) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Internal Server Error')
   }
-
-  res.writeHead(404);
-  res.end(JSON.stringify({ error: `Requested resource ${req.url} not found` }));
 }
